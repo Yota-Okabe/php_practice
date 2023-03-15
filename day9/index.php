f532abfd0c3e7716d6a5ba84ae932d5a29d1686e
Yota Okabe <93124377+Yota-Okabe@users.noreply.github.com>
2023-03-15 11:06:02 +0900

    php導入_プログラミングしてみた

--- day9/index.php
+++ day9/index.php
@@ -1,3 +1,23 @@
 <?php
 
     echo 'hello php world' ;
+    $num = 1000 * 1.1;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <h1>合計金額<?php echo $num ?></h1>
+        <form action=""></form>
+            <input type="text">
+            <input type="button" value="送信">
+</body>
+</html>
