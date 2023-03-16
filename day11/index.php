921756f01f83790571c0c2988ec2d6365e39e324
Yota Okabe <93124377+Yota-Okabe@users.noreply.github.com>
2023-03-16 23:27:26 +0900

    宿題『switch文』

--- day11/index.php
+++ day11/index.php
@@ -5,7 +5,7 @@
         // [0,1,2] 0番から数字が振られていく。
         // 下記、バナナを取り出したかったら、0を指定する。
     $fruits = ['バナナ', 'リンゴ', 'スイカ'];
-    echo $fruits[0];
+    echo $fruits[0] , '<br>';
 
     // 連想配列
         // イメージは鍵付きのタンス
@@ -20,5 +20,23 @@
     $students = ['ゴン', 'キルア', 'ズシ'];
     foreach($students as $student){
         echo $student . '<br>';
+    }
+
+    // 宿題『switch文』
+    $var = 'male';
+
+    switch ($var) {
+    case 'male':
+      // 処理
+    echo '男性です';
+    break;
+    case 'female':
+      // 処理
+    echo '女性です';
+    break;
+    default:
+      // 処理
+    echo '？';
+    break;
     }
 ?>
